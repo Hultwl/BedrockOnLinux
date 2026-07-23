65d31070b4555a1bf075adfc5f9f0c52f8e7259a
Hultwl <husseinbinmahfouz@proton.me>
2026-07-23 14:03:55 +0300

    Remove unused import of os in test_relocation.py

--- tests/test_relocation.py
+++ tests/test_relocation.py
@@ -10,7 +10,6 @@ _project_root = Path(__file__).resolve().parent.parent
 if str(_project_root) not in sys.path:
     sys.path.insert(0, str(_project_root))
 
-import os
 import pytest
 from bol import config
 
